bfae506610a873c46b3312d9c5d9fd0550ecc612
youyou-ku <73025744+youyou-ku@users.noreply.github.com>
2020-12-24 21:30:13 +0900

    サイコロ生成 (#9)
    * dice create
    
    * dice complete

---
+++ minigamon/dice.py
@@ -0,0 +1,10 @@
+def dice():
+    import random
+    a = random.randint(1, 4)
+    b = random.randint(1, 4)
+    print(f"dice:{a} {b}")
+    if a == b:
+        print("Fantastic!! You can move three times")
+        return [a, a, a]
+    else:
+        return [a, b]
