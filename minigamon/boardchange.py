e9248fc92339857eab3c00e2d5f76a27022aad33
youyou-ku <73025744+youyou-ku@users.noreply.github.com>
2021-01-07 19:41:30 +0900

    盤面変更の処理 (#11)
    * boardchangefunction create
    
    * doc and unit test create
    
    * CI create
    
    * boardchange func improve
    
    * test improve
    
    * test improve2

---
+++ minigamon/boardchange.py
@@ -0,0 +1,35 @@
+def boardchange(board, t, n: int, a: int):
+    """
+    >>> boardchange([0, 2, 2, 2, 2, -2, -2, -2, -2, 0], 1, 3, 1)
+    [0, 2, 2, 1, 3, -2, -2, -2, -2, 0]
+    >>> boardchange([0, -3, 0, 0, 3, 2, -1, 0], -1, 1, 100)
+    [0, -4, 0, 0, 3, 2, 0, 0]
+    >>> boardchange([0, 2, 2, 2, 2, -2, -2, -1, -2, -1], 1, 4, 3)
+    [0, 2, 2, 2, 1, -2, -2, 1, -2, -2]
+    >>> boardchange([0, -3, 0, 0, 3, 1, -1, 0], -1, 1, 1)
+    [1, -3, 0, 0, 3, -1, 0, 0]
+    """
+    l = len(board)-2
+    if t == 1:
+        board[n] -= 1
+        if n + a <= l:
+            p = n + a
+        else:
+            p = -2
+        if board[p] == -1:
+            board[-1] -= 1
+            board[p] = 1
+        elif board[p] >= 0:
+            board[p] += 1
+    elif t == -1:
+        board[-1-n] += 1
+        if n + a <= l:
+            p = -1-n-a
+        else:
+            p = 1
+        if board[p] == 1:
+            board[0] += 1
+            board[p] = -1
+        elif board[p] <= 0:
+            board[p] -= 1
+    return board
