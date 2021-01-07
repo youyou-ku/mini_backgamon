b9916d34e57b7b1f0b31621989d613ca33372ca4
youyou-ku <73025744+youyou-ku@users.noreply.github.com>
2021-01-07 20:09:14 +0900

    勝利判定 (#14)
    * winnercheck create
    
    * winnercheck test create

---
+++ minigamon/winnermake.py
@@ -0,0 +1,21 @@
+def winnercheck(board, n, t):
+    """
+    >>> winnercheck([0, -3, -4, -1, 0, 0, 2, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0], 8, -1)
+    True
+    >>> winnercheck([0, -3, -4, -1, 0, 0, 2, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0], 10, -1)
+    False
+    >>> winnercheck([0, -3, -4, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3, 0], 8, 1)
+    True
+    """
+    if t == 1:
+        for i in range(3):
+            if board[-2-i] > 0:
+                n -= board[-2-i]
+    elif t == -1:
+        for i in range(3):
+            if board[1+i] < 0:
+                n += board[1+i]
+    if n == 0:
+        return True
+    else:
+        return False
